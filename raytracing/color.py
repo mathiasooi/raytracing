37122a2696b094152249f2edd06587e601a32458
Mathias Ooi <mathias7142@gmail.com>
2023-04-15 18:28:07 -0700

    Include samples and clamping

--- raytracing/color.py
+++ raytracing/color.py
@@ -1,7 +1,10 @@
 from .vec3 import Vec3
+from .utility import clamp
 
 class Color(Vec3):
-    def __init__(self, e0: float = 0, e1: float = 0, e2: float = 0):
+    def __init__(self, e0: float = 0, e1: float = 0, e2: float = 0, samples = 1):
         super().__init__(e0, e1, e2)
+        self._samples = samples
+
     def __str__(self):
-        return "{} {} {}".format(*[int(255.999 * hue) for hue in self._e])
+        return "{} {} {}".format(*[int(255.999 * clamp(hue * 1.0 / self._samples, 0, 0.999)) for hue in self._e])
