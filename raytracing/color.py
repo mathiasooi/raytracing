c03c0d2a0e7e029fb020044d610cc5a29aa7a123
mathiasooi <mathias7142@gmail.com>
2023-05-02 17:11:04 -0700

    Add lambertian reflection

--- raytracing/color.py
+++ raytracing/color.py
@@ -1,5 +1,6 @@
 from .vec3 import Vec3
 from .utility import clamp
+from math import sqrt
 
 class Color(Vec3):
     def __init__(self, e0: float = 0, e1: float = 0, e2: float = 0, samples = 1):
@@ -7,4 +8,7 @@ class Color(Vec3):
         self._samples = samples
 
     def __str__(self):
-        return "{} {} {}".format(*[int(255.999 * clamp(hue * 1.0 / self._samples, 0, 0.999)) for hue in self._e])
+        # Divide the color by the number of samples and gamma-correct for gamma=2.0.
+        scale = 1.0 / self._samples
+        hues = [sqrt(scale * e) for e in self._e]
+        return "{} {} {}".format(*[int(256 * clamp(hue, 0, 0.999)) for hue in hues])
